dbead770e5abe6dceb0e48981d291cbed3dfe983
17BAN <17ban.1024@gmail.com>
2021-01-23 04:50:28 +0800

    feat(bin/www.js): make HTTPS service available
    Allow to start HTTPS service by launch argument "--https".

--- bin/www.js
+++ bin/www.js
@@ -1,12 +1,8 @@
-const http = require('http')
-
 // 引入 koa app 实例
 const app =  require('../src/app')
 
-
 // 处理启动参数
-let httpPort = null
-let httpsPort = null
+let httpPort, httpsPort, sslCertPath, sslKeyPath
 let args = process.argv.splice(2)
 for(let i = 0; i < args.length; i++) {
     let arg = args[i]
@@ -14,23 +10,34 @@ for(let i = 0; i < args.length; i++) {
         httpPort = args[++i]
     } else if(arg === '--https') {
         httpsPort = args[++i]
+        sslCertPath = args[++i]
+        sslKeyPath = args[++i]
     }
 }
 
-
-// 启动 http 服务
-if(httpPort) {
-    const httpServer = http.createServer(app.callback())
-    httpServer.listen(httpPort)
-    console.log(`HTTP service listening on Port:${httpPort}.`)
-}
-
-
 // 启动 https 服务
 if(httpsPort) {
+    if(!(sslCertPath && sslKeyPath)) {
+        throw new Error(`Can't start HTTPS service without SSL certificate or private key.`)
+    }
+    const https = require('https')
+    const fs = require('fs')
+    const httpsServer = https.createServer({
+        allowHTTP1: true,
+        cert: fs.readFileSync(sslCertPath),
+        key: fs.readFileSync(sslKeyPath)
+    }, app.callback())
+    httpsServer.listen(httpsPort)
     console.log(`HTTPS service listening on Port:${httpsPort}.`)
 }
 
+// 启动 http 服务
+if(httpPort || (!httpPort && !httpsPort)) {
+    const http = require('http')
+    const httpServer = http.createServer(app.callback())
+    httpServer.listen(httpPort || 80)
+    console.log(`HTTP service listening on Port:${httpPort || 80}.`)
+}
 
 // 捕获意料之外的异常
 process.on('uncaughtException', err => {
